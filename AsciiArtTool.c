f23d95062efa562f040adcbf3ad6b069bf6b6bb0
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-04-21 10:16:51 +0300

    Create AsciiArtTool.c
    יונתן מעלה את פונקציות 2 ו 3 
    הן עוד לא עובדות יש בעיה בפרמטרים של 
    fputs

---
+++ AsciiArtTool.c
@@ -0,0 +1,50 @@
+
+RLEListResult asciiArtPrint(RLEList list, FILE *out_stream) {
+    FILE *output = fopen(out_stream, "w");
+    if (!output) {
+        error("Error: cannot open", out_stream);
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+
+    // if (!list || out_stream == NULL)
+    //   return RLE_LIST_NULL_ARGUMENT;
+
+    RLEListResult result;
+    char *toString_RLE; //////////////////
+    toString_RLE = RLEListExportToString(list, &result);
+    if (result != RLE_LIST_SUCCESS) {
+        fclose(out_stream);
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    fputs(toString_RLE, out_stream);
+    fclose(out_stream);
+    return RLE_LIST_SUCCESS;
+}
+
+
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream) {
+    if (!list || out_stream == NULL)
+        return RLE_LIST_NULL_ARGUMENT;
+
+    FILE *output = fopen(out_stream, "w");
+    if (!output) {
+        printf("Error: cannot open");
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+
+    int index = 0;
+    RLEListResult result;
+    while (list) {
+        // if (!list || out_stream == NULL)
+        //   return RLE_LIST_NULL_ARGUMENT;
+        int character_repetition = RLEListRepetitions(list, index, &result);
+        assert(character_repetition ==-1);
+        char* current_character = (char*)(RLEListGet(list,index,&result));
+            for (int i = 0; i < character_repetition; i++) {
+                fputs(current_character, out_stream);
+            }
+        index++;
+    }
+    fclose(out_stream);
+    return RLE_LIST_SUCCESS;
+}
