f4dcc2e4ce99d538a4a6fe08c5386ab4d26b645c
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-04-21 10:33:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,15 @@ int main() {
     RLEListResult result;
     RLEList head= RLEListCreate();
     char input;
+    printf("first try: %d \n", RLEListRepetitions(head,0,&result));
     printf("enter input:\n");
     while (scanf("\n%c",&input) && input!='0') {
         result=RLEListAppend(head,input);
     }
     putchar(RLEListGet(head,0,&result));
+    printf("before: %d \n", RLEListRepetitions(head,0,&result));
     RLEListRemove(head,0);
+    printf("after: %d \n", RLEListRepetitions(head,0,&result));
     putchar('\n');
     printf("the size is: %d\n",RLEListSize(head));
     char* tos =RLEListExportToString(head,&result);
