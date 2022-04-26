c58a12cd57b3d00727f90184d9b4789a4e4ce80d
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-04-26 20:06:02 +0300

    map הוספתי דיפיין והגדרתי הסבר לפונקציה של

--- AsciiArtTool.h
+++ AsciiArtTool.h
@@ -1,9 +1,32 @@
 #ifndef HW1_ASCIIARTTOOL_H
 #define HW1_ASCIIARTTOOL_H
 
-#include <stdio.h>
-#include <stdbool.h>
+
 #include "RLEList.h"
+
+#ifndef FLAG_ARGUMENT
+#define FLAG_ARGUMENT 1
+#define SOURCE_ARGUMENT 2
+#define TARGET_ARGUMENT 3
+#define ARGUMENT_NUMBER 4
+#define ENCODED_SYMBOL "-e"
+#define INVERTED_SYMBOL "-i"
+#define CHARACTER_TO_CHANGE ' '
+#define CHANGED_CHARACTER '@'
+#define READ_PERMISSION "r"
+#define WRITE_PERMISSION "w"
+#endif
+
+
+/**    Map_function_aux
+ * The function gets a character and changes it if needed.
+ *
+ * @param list_character - character that is being checked.
+ * @return
+ *    @ if the list_character is space (' '), if not the character does not changed
+ */
+char Map_function_aux(char list_character);
+
 
 /**
  * The function reads the given file and compresses it using the RLE method.
@@ -12,7 +35,7 @@
  * @return 
  *    RLEList list which containes all of the picture's chars 
  */
-RLEList asciiArtRead(FILE* in_stream);
+RLEList asciiArtRead(FILE *in_stream);
 
 /**
  * The function writes an image that is represented by an RLEList object to a file.
