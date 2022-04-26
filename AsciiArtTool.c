162a8c28522196f2e8e0b0664b33e86b16bd4a4e
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-04-26 20:04:28 +0300

    עדכון של הקובץ, הפונקציות עובדות

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -1,61 +1,102 @@
-#include <stdio.h>
 #include "AsciiArtTool.h"
-#include <stdlib.h>
-RLEList asciiArtRead(FILE* in_stream){
-    RLEList compressed_file = RLEListCreate();
-    char input_char_value = fgetc(in_stream);
-    while (input_char_value != EOF)
+
+int main(int argc, char **argv) 
+{
+    if (argc == ARGUMENT_NUMBER) 
     {
-        RLEListAppend(compressed_file, input_char_value);
-        input_char_value = fgetc(in_stream);
+        FILE *input = fopen(argv[SOURCE_ARGUMENT], READ_PERMISSION); /// is w a magic number?
+        if (!input) 
+        {
+            return 1;
+        }
+        FILE *output = fopen(argv[TARGET_ARGUMENT], WRITE_PERMISSION); /// is w a magic number?
+        if (!output) 
+        {
+            fclose(input);
+            return 1;
+        }
+
+        RLEList list = asciiArtRead(input);
+        if (list == NULL) 
+        {
+            fclose(output);
+            fclose(input);
+            return 1;
+        }
+        if (strcmp(argv[FLAG_ARGUMENT], ENCODED_SYMBOL) == 0) 
+        {
+            asciiArtPrintEncoded(list, output);
+        }
+        else if (strcmp(argv[FLAG_ARGUMENT], INVERTED_SYMBOL) == 0) {
+            if (RLEListMap(list, Map_function_aux) == RLE_LIST_NULL_ARGUMENT) 
+            {
+                return 1;
+            }
+            asciiArtPrint(list, output);
+        }
+        fclose(output);
+        RLEListDestroy(list);
     }
-    return compressed_file;
+  
+    return 0;
 }
-RLEListResult asciiArtPrint(RLEList list, FILE *out_stream) {
-    FILE *output = fopen(out_stream, "w");
-    if (!output) {
-        error("Error: cannot open", out_stream);
-        return RLE_LIST_NULL_ARGUMENT;
-    }
 
-    // if (!list || out_stream == NULL)
-    //   return RLE_LIST_NULL_ARGUMENT;
 
-    RLEListResult result;
-    char *toString_RLE; //////////////////
-    toString_RLE = RLEListExportToString(list, &result);
-    if (result != RLE_LIST_SUCCESS) {
-        fclose(out_stream);
-        return RLE_LIST_NULL_ARGUMENT;
+// map function
+/// can it get a null?
+char Map_function_aux(char list_character) 
+{
+    if (list_character == CHARACTER_TO_CHANGE) 
+    {
+        return CHANGED_CHARACTER;
     }
-    fputs(toString_RLE, out_stream);
-    fclose(out_stream);
-    return RLE_LIST_SUCCESS;
+    return list_character;
 }
 
-RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream) {
-    if (!list || out_stream == NULL)
-        return RLE_LIST_NULL_ARGUMENT;
 
-    FILE *output = fopen(out_stream, "w");
-    if (!output) {
-        printf("Error: cannot open");
+RLEListResult asciiArtPrint(RLEList list, FILE *out_stream) 
+{
+    if (!list || out_stream == NULL) 
+    {
         return RLE_LIST_NULL_ARGUMENT;
     }
 
     int index = 0;
     RLEListResult result;
-    while (list) {
-        // if (!list || out_stream == NULL)
-        //   return RLE_LIST_NULL_ARGUMENT;
+    int size = RLEListSize(list);
+    while (index < size) 
+    {
         int character_repetition = RLEListRepetitions(list, index, &result);
-        assert(character_repetition ==-1);
-        char* current_character = (char*)(RLEListGet(list,index,&result));
-            for (int i = 0; i < character_repetition; i++) {
-                fputs(current_character, out_stream);
-            }
-        index++;
+        assert(character_repetition == -1); //////////
+        char current_character = (RLEListGet(list, index, &result));
+        char *pCurrent_character = &current_character;
+        for (int i = 0; i < character_repetition; i++) 
+        {
+            fputs(pCurrent_character, out_stream);
+        }
+        index += character_repetition;
     }
-    fclose(out_stream);
     return RLE_LIST_SUCCESS;
+}
+
+
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream) 
+{
+    RLEListResult result;
+    char *toString_RLE = RLEListExportToString(list, &result);
+    fputs(toString_RLE, out_stream);
+    return RLE_LIST_SUCCESS;
+}
+
+
+RLEList asciiArtRead(FILE *in_stream) 
+{
+    RLEList compressed_file = RLEListCreate();
+    char input_char_value = fgetc(in_stream);
+    while (input_char_value != EOF) 
+    {
+        RLEListAppend(compressed_file, input_char_value);
+        input_char_value = fgetc(in_stream);
+    }
+    return compressed_file;
 }
