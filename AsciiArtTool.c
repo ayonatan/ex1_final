8d7dcb58f27c8168f7660fb792b5667ce3b9ccff
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-04-28 16:35:57 +0300

    סופי

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -1,62 +1,8 @@
 #include "AsciiArtTool.h"
 
-int main(int argc, char **argv) 
+RLEListResult asciiArtPrint(RLEList list, FILE *outStream)
 {
-    if (argc == ARGUMENT_NUMBER) 
-    {
-        FILE *input = fopen(argv[SOURCE_ARGUMENT], READ_PERMISSION); /// is w a magic number?
-        if (!input) 
-        {
-            return 1;
-        }
-        FILE *output = fopen(argv[TARGET_ARGUMENT], WRITE_PERMISSION); /// is w a magic number?
-        if (!output) 
-        {
-            fclose(input);
-            return 1;
-        }
-
-        RLEList list = asciiArtRead(input);
-        if (list == NULL) 
-        {
-            fclose(output);
-            fclose(input);
-            return 1;
-        }
-        if (strcmp(argv[FLAG_ARGUMENT], ENCODED_SYMBOL) == 0) 
-        {
-            asciiArtPrintEncoded(list, output);
-        }
-        else if (strcmp(argv[FLAG_ARGUMENT], INVERTED_SYMBOL) == 0) {
-            if (RLEListMap(list, Map_function_aux) == RLE_LIST_NULL_ARGUMENT) 
-            {
-                return 1;
-            }
-            asciiArtPrint(list, output);
-        }
-        fclose(output);
-        RLEListDestroy(list);
-    }
-  
-    return 0;
-}
-
-
-// map function
-/// can it get a null?
-char Map_function_aux(char list_character) 
-{
-    if (list_character == CHARACTER_TO_CHANGE) 
-    {
-        return CHANGED_CHARACTER;
-    }
-    return list_character;
-}
-
-
-RLEListResult asciiArtPrint(RLEList list, FILE *out_stream) 
-{
-    if (!list || out_stream == NULL) 
+    if (!list || outStream == NULL)
     {
         return RLE_LIST_NULL_ARGUMENT;
     }
@@ -64,39 +10,39 @@ RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
     int index = 0;
     RLEListResult result;
     int size = RLEListSize(list);
-    while (index < size) 
+    while (index < size)
     {
-        int character_repetition = RLEListRepetitions(list, index, &result);
-        assert(character_repetition == -1); //////////
-        char current_character = (RLEListGet(list, index, &result));
-        char *pCurrent_character = &current_character;
-        for (int i = 0; i < character_repetition; i++) 
+        int characterRepetition = RLEListRepetitions(list, index, &result);
+        char characterRepetition = (RLEListGet(list, index, &result));
+        char *pCurrentCharacter = &currentCharacter;
+        for (int i = 0; i < characterRepetition; i++)
         {
-            fputs(pCurrent_character, out_stream);
+            fputs(pCurrentCharacter, outStream);
         }
-        index += character_repetition;
+        index += characterRepetition;
     }
     return RLE_LIST_SUCCESS;
 }
 
 
-RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream) 
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *outStream)
 {
     RLEListResult result;
-    char *toString_RLE = RLEListExportToString(list, &result);
-    fputs(toString_RLE, out_stream);
+    char *toStringRLE = RLEListExportToString(list, &result);
+    fputs(toStringRLE, outStream);
     return RLE_LIST_SUCCESS;
 }
 
 
-RLEList asciiArtRead(FILE *in_stream) 
+RLEList asciiArtRead(FILE *inStream)
 {
-    RLEList compressed_file = RLEListCreate();
-    char input_char_value = fgetc(in_stream);
-    while (input_char_value != EOF) 
+    RLEList compressedFile = RLEListCreate();
+    char inputCharValue = fgetc(inStream);
+    while (inputCharValue != EOF)
     {
-        RLEListAppend(compressed_file, input_char_value);
-        input_char_value = fgetc(in_stream);
+        RLEListAppend(compressedFile, inputCharValue);
+        inputCharValue = fgetc(inStream);
     }
-    return compressed_file;
+    return compressedFile;
 }
+
